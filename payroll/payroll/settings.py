2ae43e2798c6ae69c8179a8393ff318293b9b77c
andela-sjames <samuel.james@andela.com>
2019-08-17 22:46:44 +0000

    Update compose file

--- payroll/payroll/settings.py
+++ payroll/payroll/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'payapp.apps.PayrollConfig',
+    'payapp.apps.PayappConfig',
 ]
 
 MIDDLEWARE = [
